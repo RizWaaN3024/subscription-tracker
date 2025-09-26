cccc3da38fbe63b5d2fcb9de3b94d46f09433cd2
RizWaaN3024 <rizwaanans786@gmail.com>
2025-09-26 20:09:04 +0530

    Fix

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import subscriptionRouter from './routes/subscriptions.route.js';
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
+
 app.use('/api/v1/users', userRouter);
+
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.listen(PORT, () => {
